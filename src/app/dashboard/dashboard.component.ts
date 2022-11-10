7b3ad758806a27481059bb96a7a2145aa83cd8c8
geevar jacob <geevarjacob123@gmail.com>
2022-11-10 13:42:20 +0530

    added Deposit and Withdraw Form in dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  acn: any
+  psw: any
+  amnt: any
 
-  constructor() { }
+  acn1: any
+  psw1: any
+  amnt1: any
+
+  constructor(private ds: DataService) { }
 
   ngOnInit(): void {
+  }
+  deposit() {
+    var acn = this.acn
+    var psw = this.psw
+    var amnt = this.amnt
+
+    const result = this.ds.deposit(acn, psw, amnt)
+
+    if (result) {
+      alert(`${amnt} is credited in your Account & the available balance is Rs.${result}`)
+    }
+  }
+  withdraw() {
+    var acn1 = this.acn1
+    var psw1 = this.psw1
+    var amnt1 = this.amnt1
+    
+    const result=this.ds.withdraw(acn1,psw1,amnt1)
+
+    if(result){
+      alert(`${amnt1} is debited from your Account & the available balance is Rs.${result}`)
+
+    }
   }
 
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,53 +1,104 @@
 import { Injectable } from '@angular/core';
+import { retry } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
-  
+
   // redendent data
-  
-  userDetails:any={
-    1000:{acn:1000,username:"amal",password:123,balance:0},
-    1001:{acn:1001,username:"anu",password:123,balance:0},
-    1002:{acn:1002,username:"arun",password:123,balance:0},
-    1003:{acn:1003,username:"mega",password:123,balance:0}
+
+  userDetails: any = {
+    1000: { acn: 1000, username: "amal", password: 123, balance: 0 },
+    1001: { acn: 1001, username: "anu", password: 123, balance: 0 },
+    1002: { acn: 1002, username: "arun", password: 123, balance: 0 },
+    1003: { acn: 1003, username: "mega", password: 123, balance: 0 }
   }
 
   constructor() { }
 
   // storing values here instead of creating it in the register ts bcause these fn has direct relation with data base
 
-  register(acn:any,username:any,password:any)
-  {
-    var userDetails=this.userDetails
+  // register form logic
 
-    if(acn in userDetails){
+  register(acn: any, username: any, password: any) {
+    var userDetails = this.userDetails
+
+    if (acn in userDetails) {
       return false
       // alert('User exist')
     }
-    else{
-      userDetails[acn]={acn,username,password,balance:0}
+    else {
+      userDetails[acn] = { acn, username, password, balance: 0 }
       console.log(userDetails);
       return true
     }
   }
 
+  // Login Form logic
 
-  login(acn:any,psw:any){
-    var userDetails=this.userDetails
-  
-    if(acn in userDetails){     //to check acn is present in userDetailes
-      if(psw==userDetails[acn]['password']){  //checking entered password with the password in database -we must use[]to take data of class
+  login(acn: any, psw: any) {
+    var userDetails = this.userDetails
+
+    if (acn in userDetails) {     //to check acn is present in userDetailes
+      if (psw == userDetails[acn]['password']) {  //checking entered password with the password in database -we must use[]to take data of class
         return true
       }
-      else{
+      else {
         alert('Incorrect Password')
         return false
       }
     }
-    else{
+    else {
       alert('User not exist')
+      return false
+    }
+  }
+
+  // Deposit form logic
+
+  deposit(acn: any, psw: any, amnt: any) {
+    let userDetails = this.userDetails
+    // convert string type amnt from input into integar by using parseInt
+    var amount = parseInt(amnt)   //converting string into int
+    if (acn in userDetails) {  //checking scn in userdetailes
+      if (psw == userDetails[acn]['password']) {   //checking password in acn of userdetails
+        userDetails[acn]['balance'] += amount  //updating balance by adding deposited amount 
+        return userDetails[acn]['balance']   //returning updated balace
+      }
+      else {
+        alert('Incorrect Password')
+        return false
+      }
+    }
+    else {
+      alert("Incorrect Acc/No.")
+      return false
+    }
+  }
+
+  // Withdraw Form logic
+  withdraw(acn: any, psw: any, amnt: any) {
+    let userDetails = this.userDetails
+    var amount = parseInt(amnt)
+    if (acn in userDetails) {
+      if (psw == userDetails[acn]['password']) {
+        if (amount <= userDetails[acn]['balance']){
+          userDetails[acn]['balance'] -= amount
+          return userDetails[acn]['balance']
+        }
+        else{
+          alert('Insufficient Balance')
+          return false
+        }
+      }
+      else {
+        alert('Incorrect password')
+        return false
+      }
+    }
+    else {
+      alert('Incorrect Acc/No.')
       return false
     }
   }
